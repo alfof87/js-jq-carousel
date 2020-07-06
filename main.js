fe1771f190e7b72a0ec6b789821295da10dd7e85
Alfonso <alfof87@gmail.com>
2020-07-06 09:17:55 +0200

    js-jq-carousel

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
 $(document).ready(function(){
 $(".next").click(function(){
-  $(".slider-wrapper img.active").removeClass("active");
-  $(".slider-wrapper img.active").next().addClass("active");
+  var j = $(".slider-wrapper img.active");
+  j.removeClass("active");
+  var k = $(".slider-wrapper i.active");
+  k.removeClass("active");
+
+
+  if (j.hasClass("last") == true) {
+    $(".slider-wrapper img.first").addClass("active");
+    $(".slider-wrapper i.first").addClass("active");
+  }else{
+      j.next().addClass("active");
+      k.next().addClass("active");
+  }
 });
+////////////////////////////////////////////////////////////
+$(".prev").click(function(){
+  var j = $(".slider-wrapper img.active");
+  j.removeClass("active");
+  var k = $(".slider-wrapper i.active");
+  k.removeClass("active");
+
+
+  if (j.hasClass("first") == true) {
+    $(".slider-wrapper img.last").addClass("active");
+    $(".slider-wrapper i.last").addClass("active");
+  }else{
+      j.prev().addClass("active");
+      k.prev().addClass("active");
+  }
+});
+
 
 
 
